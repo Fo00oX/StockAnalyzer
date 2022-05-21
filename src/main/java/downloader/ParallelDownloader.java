1f75ac162705593d905f777469e0414bfb0a3bf8
Markant <mark.perov@stud.fh-campuswien.ac.at>
2022-05-21 11:03:12 +0200

    Downloader and Exceptions Finish

---
+++ src/main/java/downloader/ParallelDownloader.java
@@ -0,0 +1,39 @@
+package downloader;
+
+import stockanalyzer.ctrl.YahooException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ParallelDownloader extends Downloader {
+
+    @Override
+    public int process(List<String> urls) {
+
+        int counter = 0;
+        int threads = Runtime.getRuntime().availableProcessors();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(threads);
+
+        List<Future<String>> parallelDownload = new ArrayList<>();
+
+        for(String url: urls){
+            parallelDownload.add(executorService.submit(()->saveJson2File(url)));
+        }
+
+        for(Future<String> futures : parallelDownload){
+            try {
+                String list = futures.get();
+
+                if(list != null){
+                    counter++;
+                }
+
+            } catch ( InterruptedException | ExecutionException e) {
+                System.out.println ("here is no Data available in Parallel downloader." );
+            }
+        }
+        return counter;
+    }
+}
